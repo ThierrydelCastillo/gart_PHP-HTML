4683604bd693b6bc69a3eac7ab682f33272a1f34
ThierrydelCastillo <dev@del-castillo.fr>
2022-05-10 16:32:02 +0200

    20 - TP : Dashboard

--- functions/compteur.php
+++ functions/compteur.php
@@ -21,6 +21,36 @@ function nombre_vues(): string {
     return file_get_contents($fichier);
 }
 
+function nombre_vues_mois(int $annee, int $mois):int {
+    $mois = str_pad($mois, 2, '0', STR_PAD_LEFT);
+    $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur-' . $annee . '-' . $mois . '-' . '*';
+    $fichiers = (glob($fichier));
+    $total = 0;
+    foreach ($fichiers as $fichier) {
+        $total += (int)file_get_contents($fichier);
+    }
+    return $total;
+}
+
+function nombre_vues_detail_mois(int $annee, int $mois):array {
+    $mois = str_pad($mois, 2, '0', STR_PAD_LEFT);
+    $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur-' . $annee . '-' . $mois . '-' . '*';
+    $fichiers = (glob($fichier));
+    $visites = [];
+    foreach ($fichiers as $fichier) {
+        $parties = explode('-', basename($fichier));
+        $visites[] = [
+            'annee' => $parties[1],
+            'mois' => $parties[2],
+            'jour' => $parties[3],
+            'visites' => file_get_contents($fichier)
+        ];
+    }
+    return $visites;
+}
 
 // FONCTIONS
 // file_exists — Vérifie si un fichier ou un dossier existe
+// str_pad — Complète une chaîne jusqu'à une taille donnée
+// glob — Recherche des chemins qui vérifient un masque
+// basename — Retourne le nom de la composante finale d'un chemin
