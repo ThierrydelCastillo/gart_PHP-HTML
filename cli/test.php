fee88efe6d2bd89fe065982188ab4b54e8896a2c
ThierrydelCastillo <dev@del-castillo.fr>
2023-05-10 17:26:33 +0200

    23 - L'objet DateTime 02

--- cli/test.php
+++ cli/test.php
@@ -1,15 +1,5 @@
 <?php
-$date = '2014-01-01';
-$date2= '2019-04-01';
-
-$d = new DateTime($date);
-$d2 = new DateTime($date2);
-$diff = $d->diff($d2, true);
-echo "il y a $diff->y années, $diff->m mois et $diff->d jours de différence";
-
-echo "\n";
-
-$time = strtotime($date);
-$time2= strtotime($date2);
-$days= floor(abs(($time2 - $time) / (24 * 60 * 60)));
-echo "il y a $days jours de différence";
+$date = new DateTime('2019-01-01');
+$interval = new DateInterval('P1M1DT1M');
+$date->add($interval);
+var_dump($date);
