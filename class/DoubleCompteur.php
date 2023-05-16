c7fe7c5361ed87a023c7d435b23dfd4d745f9954
ThierrydelCastillo <dev@del-castillo.fr>
2023-05-16 16:02:02 +0200

    delete pour prochain cours

--- class/DoubleCompteur.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Compteur.php';
-
-class DoubleCompteur extends Compteur
-{
-   protected const INCREMENT = 10;
-}
--- cli/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Form.php';
-echo Form::$class . PHP_EOL; // Appel d'une propriété static
-echo Form::checkbox('demo', 'Demo'); // appel d'une méthode static
